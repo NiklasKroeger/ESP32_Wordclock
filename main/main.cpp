07de399501f315b810a681e7a82d1de095699df5
Niklas Kröger <kroeger.niklas@gmail.com>
2018-01-21 16:29:15 +0100

    Move to cpp and start use logging instead of print

--- main/main.cpp
+++ main/main.cpp
@@ -1,3 +1,4 @@
+#include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include "esp32_digital_led_lib.h"
@@ -14,8 +15,23 @@ strand_t STRANDS[] = {
 
 int STRANDCNT = sizeof(STRANDS) / sizeof(STRANDS[0]);
 
-void app_main() {
-	ets_printf("Initializing strands...\n");
+void print_mask(bool mask[], int len) {
+	for (int i = 0; i < len; i++) {
+		ets_printf("%d ", mask[i]);
+	}
+	ets_printf("\n");
+}
+
+extern "C" {
+	int app_main();
+}
+
+int app_main() {
+	ESP_LOGI("app_main", "*********************");
+	ESP_LOGI("app_main", "* starting app_main *");
+	ESP_LOGI("app_main", "*********************");
+
+	ESP_LOGI("app_main", "Initializing %d strand(s)...", STRANDCNT);
 	digitalLeds_initStrands(STRANDS, STRANDCNT);
 
 	// define the on and off color for the leds and get a pointer to the
@@ -23,13 +39,21 @@ void app_main() {
 	pixelColor_t color1 = pixelFromRGB(50, 50, 50);
 	pixelColor_t colorOff = pixelFromRGB(0, 0, 0);
 	strand_t * pStrand = &STRANDS[0];
+
+	// Try and set some LEDs on by using a mask
+	bool mask[pStrand->numPixels] = { false };
+	int len = sizeof(mask) / sizeof(mask[0]);
+	print_mask(mask, len);
+
 	while (true) {
+
 		for (int i = 0; i < pStrand->numPixels; i++) {
 			pStrand->pixels[i] = color1;
 			pStrand->pixels[i-1] = colorOff;
 			digitalLeds_updatePixels(pStrand);
-			vTaskDelay(100 / portTICK_PERIOD_MS);
+			vTaskDelay(500 / portTICK_PERIOD_MS);
 		}
 		pStrand->pixels[pStrand->numPixels-1] = colorOff;
 	}
 }
+
