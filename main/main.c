58eda665e6340d1dbaa802d0b576c26e847a3e58
Niklas Kröger <kroeger.niklas@gmail.com>
2018-01-21 10:53:01 +0100

    Fix running light animation

--- main/main.c
+++ main/main.c
@@ -20,16 +20,16 @@ void app_main() {
 
 	// define the on and off color for the leds and get a pointer to the
 	// strand we want to work with
-	pixelColor_t color1 = pixelFromRGB(100, 100, 100);
+	pixelColor_t color1 = pixelFromRGB(50, 50, 50);
 	pixelColor_t colorOff = pixelFromRGB(0, 0, 0);
 	strand_t * pStrand = &STRANDS[0];
 	while (true) {
 		for (int i = 0; i < pStrand->numPixels; i++) {
-			// TODO: this always leaves the last LED on
 			pStrand->pixels[i] = color1;
 			pStrand->pixels[i-1] = colorOff;
 			digitalLeds_updatePixels(pStrand);
 			vTaskDelay(100 / portTICK_PERIOD_MS);
 		}
+		pStrand->pixels[pStrand->numPixels-1] = colorOff;
 	}
 }
